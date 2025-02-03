fdc7e453934c915455cc705632119f9a002c6120
Lidia Cirrone <lidia.cirrone@gmail.com>
2025-02-03 23:50:41 +0100

    style(nav): made navigation sticky and faded

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,15 +5,17 @@ import { Link, Outlet, useLocation } from 'react-router'
 const Layout = () => {
   const { pathname } = useLocation();
   return (
-    <main>
-      <Outlet />
+    <>
+      <main>
+        <Outlet />
+      </main>
       <nav>
         <Link to="/" className={clsx(pathname === '/' && "current")}>home</Link>
         <Link to="/about" className={clsx(pathname === '/about' && "current")}>about</Link>
         <Link to="/now" className={clsx(pathname === '/now' && "current")}>now</Link>
         <Link to="/projects" className={clsx(pathname === '/projects' && "current")}>projects</Link>
       </nav>
-    </main>
+    </>
   )
 }
 
